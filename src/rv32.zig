ea4b61975b724aae5331f29cf303ac015bdbb49d
WGrav01 <63617338+WGrav01@users.noreply.github.com>
2025-09-29 13:04:24 -0400

    Base RV32 CPU struct (no fetch/decode/execute functions yet)

---
+++ src/rv32.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+const dram = @import("dram.zig").DRAM;
+
+/// Base CPU struct for the RV32 ISA
+const Rv32 = struct {
+    /// Registers x0-x31, all 32 bit
+    registers: [32]u32,
+
+    /// The program counter
+    pc: u32,
+
+    /// The connected DRAM
+    dram: dram,
+
+    pub fn init(d: dram) Rv32 {
+        const cpu = Rv32{
+            .registers = std.mem.zeroes([32]u32),
+            .pc = d.base, // Set program counter to DRAM base
+            .dram = d,
+        };
+
+        cpu.registers[0] = 0x00; // Register x0 is hardwired to zero
+        cpu.registers[2] = d.base + d.size; // Set the stack pointer
+    }
+};
