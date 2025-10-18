bb647678abd40808ae0bd274d8d8cd8c1e8d043b
WGrav01 <63617338+WGrav01@users.noreply.github.com>
2025-10-17 22:25:53 -0400

    Replaced the decoder stuff with listings for each opcode, I will put the decoders in a seperate file once I figure out how best to plan them.

--- lib/instruction.zig
+++ lib/instruction.zig
@@ -1,59 +1,138 @@
-/// A singular decoded instruction. Use the Instructions (plural) for a vectorized structure of arrays, which is more efficent.
-pub const Instruction32 = struct {
-    opcode: u7, // TODO: refactor to use an enum later down the line
-    rd: u5,
-    funct3: u2,
-    rs1: u5,
-    rs2: u5,
-    funct7: u7,
-
-    pub inline fn decode(instruction: u32) Instruction32 {
-        return Instruction32{
-            .opcode = @truncate(instruction & 0x7f),
-            .rd = @truncate((instruction >> 7) & 0x1f),
-            .funct3 = @truncate((instruction >> 12) & 0x07),
-            .rs1 = @truncate((instruction >> 15) & 0x1f),
-            .rs2 = @truncate((instruction >> 20) & 0x1f),
-            .funct7 = @truncate((instruction >> 25) & 0x07),
-        };
-    }
+/// Helper enum for the RISC-V instruction types. They are designated by their 7 bit opcode. (bits 0 to 6 in an instruction)
+const InstructionFormat = enum(u7) {
+    R = 0b0110011,
+    I = 0b0010011,
+    S = 0b0100011,
+    B = 0b1100011,
+    J = 0b1101111,
+    U = 0b0110111,
 };
 
-/// Plural version of the Instruction32 struct, using @Vector for optimization.
-pub fn Instructions32(comptime len: comptime_int) type {
-    var initialized = struct {
-        len: usize,
-        opcode: @Vector(len, u7),
-        rd: @Vector(len, u5),
-        funct3: @Vector(len, u2),
-        rs1: @Vector(len, u5),
-        rs2: @Vector(len, u5),
-        funct7: @Vector(len, u7),
+/// An enum for RV32 Base Integer instructions, R type, or register to register instructions
+const RV32I_Instruction_R = enum {
+    /// Add:
+    /// rd = rs1 + rs2
+    add,
 
-        pub inline fn decode(instructions: @Vector(len, u32)) @This() {
-            const instruction_struct = Instructions32(instructions.len);
+    /// Sub:
+    /// rd = rs1 - rs2
+    sub,
 
-            const u7_mask = @as(@Vector(u32, instructions.len), @splat(0x7f));
-            const u5_mask = @as(@Vector(u32, instructions.len), @splat(0x1f));
-            const u3_mask = @as(@Vector(u32, instructions.len), @splat(0x07));
-            const shift_7: @Vector(len, u5) = @splat(7);
-            const shift_12: @Vector(len, u5) = @splat(12);
-            const shift_15: @Vector(len, u5) = @splat(15);
-            const shift_20: @Vector(len, u5) = @splat(20);
-            const shift_25: @Vector(len, u5) = @splat(25);
+    /// Bitwise exclusive OR:
+    /// rd = rs1 ˆ rs2
+    xor,
 
-            return instruction_struct{
-                .opcode = @truncate(instructions & u7_mask),
-                .rd = @truncate((instructions >> shift_7) & u5_mask),
-                .funct3 = @truncate((instructions >> shift_12) & u3_mask),
-                .rs1 = @truncate((instructions >> shift_15) & u5_mask),
-                .rs2 = @truncate((instructions >> shift_20) & u5_mask),
-                .funct7 = @truncate((instructions >> shift_25) & u7_mask),
-            };
-        }
-    };
+    /// Bitwise OR:
+    /// rd = rs1 | rs2
+    OR, // Uppercase because "or" is a zig keyword
 
-    initialized.len = @as(usize, @intCast(len));
+    /// Bitwise AND:
+    /// rd = rs1 & rs2
+    AND, // Likewise
 
-    return initialized;
-}
+    /// Shift left logical:
+    /// rd = rs1 << rs2
+    sll,
+
+    /// Shift right logical:
+    /// rd = rs1 >> rs2
+    srl,
+
+    /// Shift Right Arith
+    /// rd = rs1 >> rs2
+    /// msb-extends
+    sra,
+
+    /// Set Less Than
+    /// rd = (rs1 < rs2)?1:0
+    slt,
+
+    /// Set Less then (U)
+    /// rd = (rs1 < rs2)?1:0
+    /// zero-extends
+    sltu,
+};
+
+const RV32I_Instruction_I = enum {
+    /// ADD Immediate
+    /// rd = rs1 + imm
+    addi,
+
+    /// XOR Immediate
+    /// rd = rs1 ˆ imm
+    xori,
+
+    /// OR Immediate
+    /// rd = rs1 | imm
+    ori,
+
+    /// AND Immediate
+    /// rd = rs1 & imm
+    andi,
+
+    /// Shift Left Logical Imm
+    /// rd = rs1 << imm[0:4]
+    slli,
+
+    /// Shift Right Logical Imm
+    /// rd = rs1 >> imm[0:4]
+    srli,
+
+    /// Shift Right Arith Imm
+    /// rd = rs1 >> imm[0:4]
+    /// msb-extends
+    srai,
+
+    /// Set Less Than Imm
+    /// rd = (rs1 < imm)?1:0
+    slti,
+
+    /// Set Less Than Imm (u)
+    /// rd = (rs1 < imm)?1:0
+    /// zero-extends
+    sltiu,
+
+    /// Load Byte
+    /// rd = M[rs1+imm][0:7]
+    lb,
+
+    /// Load Half
+    /// rd = M[rs1+imm][0:15]
+    lh,
+
+    /// Load Word
+    /// rd = M[rs1+imm][0:31]
+    lw,
+
+    /// Load Byte (U)
+    /// rd = M[rs1+imm][0:7]
+    /// zero-extends
+    lbu,
+
+    /// Load Half (U)
+    /// rd = M[rs1+imm][0:15]
+    /// zero-extends
+    lhu,
+
+    /// Environment call
+    /// Transfer control to OS
+    ecall,
+
+    /// Environment Break
+    /// Transfer control to debugger
+    ebreak,
+};
+
+const RV32I_Instruction_S = enum {
+    /// Store Byte
+    /// M[rs1+imm][0:7] = rs2[0:7]
+    sb,
+
+    /// Store Half
+    /// M[rs1+imm][0:15] = rs2[0:15]
+    sh,
+
+    /// Store Word
+    /// M[rs1+imm][0:31] = rs2[0:31]
+    sw,
+};
